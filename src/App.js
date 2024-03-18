d58a6da578e73628215f448250e9967884ec9ec6
kamenkraychev <106027471+kamenkraychev@users.noreply.github.com>
2024-03-18 13:08:51 +0200

    Chat opens correctly

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed'
+
+import './App.css'
+
+const App = () => {
+    return (
+        <ChatEngine 
+            height="100vh"
+            projectID="70fbc5e3-4d80-4351-ba31-a2cfc2304fb8"
+            userName="kamenkr"
+            userSecret="123123"
+            renderChatFeed={(chatAppProps) => <ChatFeed { ... chatAppProps} />}
+        
+        
+        />
+    );
+};
+
+export default App;
